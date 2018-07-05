0511d375d79bc759c44e71f25dba2d076972dba0
Samson Yuen <samsony@outlook.com>
2018-07-05 01:05:23 +0000

    add form to add new campsites

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var campgrounds = [
     {name: "Salmon Creek", image: "https://www.photosforclass.com/download/pixabay-1845906?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fe83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f0c770aeecb0b8_960.jpg&user=Pexels"},
@@ -10,12 +12,23 @@ var campgrounds = [
 
 app.get("/", function(req, res){
     res.render("landing");
-})
+});
+
+app.post("/campgrounds", function (req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    campgrounds.push({name: name, image: image});
+    res.redirect("/campgrounds");
+});
 
 app.get("/campgrounds", function(req, res){
     res.render("campgrounds", {campgrounds: campgrounds});
-})
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp Server is in session!");
-})
+});
