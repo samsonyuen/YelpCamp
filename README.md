3743f42b5ea737f95fb5a66a9d7e8c93a48921f4
Samson Yuen <samsony@outlook.com>
2018-07-06 23:15:14 +0000

    add show and add description

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # YelpCamp
 
-* Add Landing Page
-* Add Campgrounds Page that list all campgrounds
 
-Each Campground has:
-* Name
-* Image
+<!--RESTFUL ROUTES-->
+
+<!--name        url             verb        Description-->
+<!--======================================================-->
+<!--INDEX     /campgrounds       GET    Display a list of all campgrounds-->
+<!--NEW       /campgrounds/new   GET    Display form to make new campgrounds-->
+<!--CREATE    /campgrounds       POST   Add new campground to DB-->
+<!--SHOW      /campgrounds/:id   GET    Shows info about a campground-->
--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 })
 
 var Campground = mongoose.model("CampGround", campgroundSchema);
@@ -19,10 +20,12 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+// CREATE - add new campground to DB
 app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampGround = {name: name, image: image}
+    var description = req.body.description;
+    var newCampGround = {name: name, image: image, description}
     Campground.create(newCampGround, function (err, campground){
     if (err){
         console.log(err);
@@ -35,19 +38,36 @@ app.post("/campgrounds", function (req, res) {
     res.redirect("/campgrounds");
 });
 
+//INDEX - Show all campgrounds
 app.get("/campgrounds", function(req, res){
     Campground.find({}, function (err, allCampgrounds){
        if (err){
            console.log (err);
        } 
        else {
-           res.render("campgrounds", {campgrounds: allCampgrounds})
+           res.render("index", {campgrounds: allCampgrounds})
        }
     });
 });
 
+// NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
+});
+
+// SHOW - show more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    // find the campground with provided ID
+    // render show template 
+    var id = req.params.id;
+    Campground.findById(id, function(err, foundCampground){
+        if (err){
+            console.log(err);
+        }
+        else {
+            res.render("show", {campground: foundCampground});
+        }
+    })
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
