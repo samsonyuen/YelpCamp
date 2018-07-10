d6c602d09c6a0484a99cdf5f8882ced99514b1ca
Samson Yuen <samsony@outlook.com>
2018-07-10 23:43:23 +0000

    add comments

--- app.js
+++ app.js
@@ -42,14 +42,14 @@ app.get("/campgrounds", function(req, res){
            console.log (err);
        } 
        else {
-           res.render("index", {campgrounds: allCampgrounds})
+           res.render("campgrounds/index", {campgrounds: allCampgrounds})
        }
     });
 });
 
 // NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 // SHOW - show more info about one campground
@@ -63,9 +63,44 @@ app.get("/campgrounds/:id", function(req, res){
             console.log(err);
         }
         else {
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
+});
+
+//==================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if (err){
+            console.log(err);
+        }
+        else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+   // look up campground using ID
+   Campground.findById(req.params.id, function(err, campground){
+      if (err){
+          console.log(err);
+          res.redirect("/campgrounds");
+      } 
+      else {
+          Comment.create(req.body.comment, function(err, comment){
+              if (err){
+                  console.log(err);
+              }
+              else {
+                  campground.comments.push(comment);
+                  campground.save();
+                  res.redirect("/campgrounds/" + campground._id);
+              }
+          });
+      }
+   });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
