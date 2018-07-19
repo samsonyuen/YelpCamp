1bdbfc5b58b0d5069bcb29fcfb8416039e6c9ead
Samson Yuen <samsony@outlook.com>
2018-07-19 03:35:04 +0000

    small change to writeline

--- app.js
+++ app.js
@@ -48,5 +48,5 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("YelpCamp Server is in session!");
+    console.log("YelpCamp Server is running!");
 });
